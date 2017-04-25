7810467395cdbb4afcf1c062796d721aad45fa31
Alyssa Moody and John Dowd <alyssa@>
2017-04-25 13:47:12 -0700

    Add functions to js file, display none to stylesheet, and content to html.

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $(".click-js").click(function() {
+    $(".hide-js").toggle();
+    $(".show-js").toggle();
+  });
+  $(".click-op").click(function() {
+    $(".hide-op").toggle();
+    $(".show-op").toggle();
+  });
+  $(".click-var").click(function() {
+    $(".hide-var").toggle();
+    $(".show-var").toggle();
+  });
+  $(".click-vnc").click(function() {
+    $(".hide-vnc").toggle();
+    $(".show-vnc").toggle();
+  });
+  $(".click-fun").click(function() {
+    $(".hide-fun").toggle();
+    $(".show-fun").toggle();
+  });
+  $(".click-me").click(function() {
+    $(".hide-me").toggle();
+    $(".show-me").toggle();
+  });
+  $(".click-arg").click(function() {
+    $(".hide-arg").toggle();
+    $(".show-arg").toggle();
+  });
+  $(".click-par").click(function() {
+    $(".hide-par").toggle();
+    $(".show-par").toggle();
+  });
+  $(".click-re").click(function() {
+    $(".hide-re").toggle();
+    $(".show-re").toggle();
+  });
+  $(".click-cm").click(function() {
+    $(".hide-cm").toggle();
+    $(".show-cm").toggle();
+  });
+  $(".click-str").click(function() {
+    $(".hide-str").toggle();
+    $(".show-str").toggle();
+  });
+  $(".click-boo").click(function() {
+    $(".hide-boo").toggle();
+    $(".show-boo").toggle();
+  });
+  $(".click-und").click(function() {
+    $(".hide-und").toggle();
+    $(".show-und").toggle();
+  });
+  $(".click-nan").click(function() {
+    $(".hide-nan").toggle();
+    $(".show-nan").toggle();
+  });
+  $(".click-esc").click(function() {
+    $(".hide-esc").toggle();
+    $(".show-esc").toggle();
+  });
+  $(".click-al").click(function() {
+    $(".hide-al").toggle();
+    $(".show-al").toggle();
+  });
+  $(".click-com").click(function() {
+    $(".hide-com").toggle();
+    $(".show-com").toggle();
+  });
+  $(".click-jq").click(function() {
+    $(".hide-jq").toggle();
+    $(".show-jq").toggle();
+  });
+  $(".click-att").click(function() {
+    $(".hide-att").toggle();
+    $(".show-att").toggle();
+  });
+});
